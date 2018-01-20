3cd80ba20bd9ddb669959521f04b3c9c91b6b47d
melonacode <wndudpower@gmail.com>
2018-01-20 22:30:07 +0900

    e

---
+++ app/src/main/java/com/example/fooddeuk/viewholder/ViewHolderWithPictureNecessary.java
@@ -0,0 +1,119 @@
+package com.example.fooddeuk.viewholder;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.fooddeuk.R;
+import com.example.fooddeuk.model.menu.MenuContentItem;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+
+/**
+ * Created by heo on 2018. 1. 20..
+ */
+
+public class ViewHolderWithPictureNecessary extends RecyclerView.ViewHolder {
+    public Context context;
+    //Layout
+    @BindView(R.id.menu_master_layout)
+    public ConstraintLayout menu_master_layout;
+    @BindView(R.id.menu_detail_layout)
+    public LinearLayout menu_detail_layout;
+
+    //Master
+    @BindView(R.id.menu_master_picture)
+    public ImageView menu_master_picture;
+    @BindView(R.id.menu_master_name)
+    public TextView menu_master_name;
+    @BindView(R.id.menu_master_price)
+    public TextView menu_master_price;
+
+    //Detail
+    @BindView(R.id.menu_detail_picture)
+    public ImageView detailHotMenuPicture;
+    @BindView(R.id.menu_detail_name)
+    public TextView menu_detail_name;
+    @BindView(R.id.menu_detail_price)
+    public TextView menu_detail_price;
+    @BindView(R.id.menu_detail_description)
+    public TextView menu_detail_description;
+    @BindView(R.id.menu_detail_rating)
+    public TextView menu_detail_rating;
+    @BindView(R.id.menu_detail_rating_bar)
+    public RatingBar menu_detail_rating_bar;
+    @BindView(R.id.menu_detail_review)
+    public TextView menu_detail_review;
+    @BindView(R.id.menu_detail_cart)
+    public Button menu_detail_cart;
+    @BindView(R.id.menu_detail_order)
+    public Button menu_detail_order;
+
+
+    @BindView(R.id.menu_detail_necessary_option_layout)
+    public RelativeLayout menu_detail_necessary_option_layout;
+
+    @BindView(R.id.menu_detail_option_necessary_title)
+    public TextView menu_detail_option_necessary_title;
+
+    @BindView(R.id.menu_detail_option_necessary_content)
+    public TextView menu_detail_option_necessary_content;
+
+
+    @BindView(R.id.menu_detail_total_price)
+    public TextView menu_detail_total_price;
+
+
+    public ViewHolderWithPictureNecessary(Context context,View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+
+    }
+
+
+    public void bind(MenuContentItem menuContentItem) {
+        Picasso.with(context).load(menuContentItem.picture).transform(new CropCircleTransformation()).into(menu_master_picture);
+        Picasso.with(context).load(menuContentItem.picture).transform(new CropCircleTransformation()).into(detailHotMenuPicture);
+        String reviewCount = "(" + menuContentItem.reviewCnt + ")";
+        String menuPrice = menuContentItem.price+"원";
+        if (menuContentItem.rating.length() == 1) {
+            menuContentItem.rating += ".0";
+        }
+        menu_master_name.setText(menuContentItem.name);
+        menu_master_price.setText(menuPrice);
+        menu_detail_name.setText(menuContentItem.name);
+        menu_detail_name.setText(menuContentItem.name);
+        menu_detail_price.setText(menuPrice);
+        menu_detail_rating.setText(menuContentItem.rating);
+        menu_detail_review.setText(reviewCount);
+        menu_detail_rating_bar.setRating(Float.parseFloat(menuContentItem.rating));
+        menu_detail_rating_bar.setStepSize(0.5f);
+        menu_detail_order.setText(menuPrice + " 바로 주문");
+
+        if (menuContentItem.description.length() > 1) {
+            menu_detail_description.setText(menuContentItem.description);
+        } else {
+            menu_detail_description.setVisibility(View.GONE);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
