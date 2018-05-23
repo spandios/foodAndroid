f89057993d65d74bfc926b371405d7c6ef2af927
spandios <wndudpower@gmail.com>
2018-05-23 18:32:31 +0900

    0523

---
+++ app/src/main/java/com/example/fooddeuk/util/NetworkUtil.kt
@@ -0,0 +1,18 @@
+package com.example.fooddeuk.util
+
+import android.app.Activity
+import android.content.Context
+import android.location.LocationManager
+import android.net.ConnectivityManager
+
+import android.content.Context.LOCATION_SERVICE
+
+/**
+ * Created by heojuyeong on 2017. 10. 1..
+ */
+
+object NetworkUtil {
+
+
+
+}
